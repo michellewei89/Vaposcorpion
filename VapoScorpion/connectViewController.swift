b0e882653219abb6630a096061868db5b84bf115
Wen Wei <wenwei@wens-macbook-pro.local>
2016-12-23 09:00:24 -0800

    connected, direct command test passed

--- VapoScorpion/connectViewController.swift
+++ VapoScorpion/connectViewController.swift
@@ -11,8 +11,32 @@ import ExternalAccessory
 
 
 class connectViewController: UIViewController {
-    
-    
+    var connection: Ev3Connection? = nil
+    var brick: Ev3Brick? = nil
+    var connectedEV3 : EAAccessory? = nil
+
+    @IBAction func ConnectButton(_ sender: Any) {
+        let complete : EABluetoothAccessoryPickerCompletion? = nil
+        EAAccessoryManager.shared().showBluetoothAccessoryPicker(withNameFilter: nil, completion: complete)
+        connectedEV3 = getEv3Accessory()
+        if connectedEV3 != nil {
+            connect(accessory: connectedEV3!)
+            print("EV3 reconnected ")
+        } else {
+            print("EV3 not connected")
+        }
+    }
+    @IBAction func forwardButton(_ sender: Any) {
+        if (brick == nil) {
+            return
+        }
+        let command : Ev3DirectCommand = Ev3DirectCommand(brick : brick!)
+        command.playTone(10, frequency: 500, duration: 2000)
+    }
+    @IBAction func backButton(_ sender: Any) {
+    }
+    @IBAction func stopButton(_ sender: Any) {
+    }
     
     func getEv3Accessory() -> EAAccessory? {
         let man = EAAccessoryManager.shared()
@@ -39,22 +63,40 @@ class connectViewController: UIViewController {
         connect(accessory: connectedAccessory)
     }
     
-    private func connect(accessory: EAAccessory){
+    func accessoryDisconnected(notification: Notification) {
+        print("EAController::accessoryDisconnected")
+        let connectedAccessory = notification.userInfo![EAAccessoryKey] as! EAAccessory
         
-        let connection: Ev3Connection? = Ev3Connection(accessory: accessory)
-        let brick: Ev3Brick? = Ev3Brick(connection: connection!)
+        // check if the device is a ev3
+        if !Ev3Connection.supportsEv3Protocol(connectedAccessory) {
+            return
+        }
+        
+        disconnect()
+    }
+    
+    private func connect(accessory: EAAccessory){
+        if connection != nil && !(connection!.isClosed) {
+            return
+        }
+        connection = Ev3Connection(accessory: accessory)
+        brick = Ev3Brick(connection: connection!)
         connection?.open()
+        print("EV3 connection successful")
     }
 
+    private func disconnect(){
+        print("EAController::EV3 accessoryDisconnected")
+        connection = nil
+        brick = nil
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let complete : EABluetoothAccessoryPickerCompletion? = nil
-        EAAccessoryManager.shared().showBluetoothAccessoryPicker(withNameFilter: nil, completion: complete)
-        
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(accessoryConnected), name: NSNotification.Name.EAAccessoryDidConnect, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(accessoryDisconnected), name: NSNotification.Name.EAAccessoryDidDisconnect, object: nil)
+        EAAccessoryManager.shared().registerForLocalNotifications()
     }
 
     override func didReceiveMemoryWarning() {
